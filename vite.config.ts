02632b12dbb2aac650d669c224ff9e5f84074f3f
Bogomil Stoynov <bogomil@bstoynov.com>
2026-01-22 19:09:44 +0200

    scaffold initial design tokens

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,20 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@styles/tokens" as *;`,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@features": path.resolve(__dirname, "./src/features"),
       "@store": path.resolve(__dirname, "./src/store"),
       "@api": path.resolve(__dirname, "./src/api"),
       "@constants": path.resolve(__dirname, "./src/constants"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
     },
   },
 });
