51c7a6d7ad8fc4218551f51fb835a6f3cc4f23a8
Bogomil Stoynov <bogomil@bstoynov.com>
2026-01-23 23:33:33 +0200

    add riskTrend api

---
+++ api/riskTrend.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  res.status(200).json({
+    yStart: 0,
+    yEnd: 40,
+    data: {
+      RiskExposure: [8, 12, 10, 15, 11, 12, 9, 14, 7, 16, 13, 10],
+      DocumentsAnalyzed: [12, 18, 22, 28, 35, 47, 32, 38, 25, 42, 30, 36],
+    },
+  });
+}
