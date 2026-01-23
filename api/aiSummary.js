a77aa1dd9fc228cfed935e76d333c98757920499
Bogomil Stoynov <bogomil@bstoynov.com>
2026-01-23 18:34:16 +0200

    cors fix for localhost

--- api/aiSummary.js
+++ api/aiSummary.js
@@ -1,4 +1,12 @@
-export default function handler(_, res) {
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   res.status(200).json({
     "riskSeverity": "Medium",
     "riskScore": 2.3,
--- api/documents.js
+++ api/documents.js
@@ -1,4 +1,12 @@
-export default function handler(_, res) {
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   res.status(200).json([
     {
       "name": "NDA_v3.2_Draft",
