ea1f6afbcb7a96c0811c0b61eb7b904110d85495
Bogomil Stoynov <bogomil@bstoynov.com>
2026-01-24 15:52:13 +0200

    add nav icons

--- src/ui/components/Icon/svgIcons.ts
+++ src/ui/components/Icon/svgIcons.ts
@@ -1,21 +1,41 @@
 import arrowCircle from "@icons/arrow-circle.svg";
+import chart from "@icons/chart.svg";
+import chevronDown from "@icons/chevron-down.svg";
+import clipboard from "@icons/clipboard.svg";
+import doc from "@icons/doc.svg";
+import dotsHorizontal from "@icons/dots-horizontal.svg";
 import download from "@icons/download.svg";
 import eye from "@icons/eye.svg";
-import dotsHorizontal from "@icons/dots-horizontal.svg";
-import pdf from "@icons/pdf.svg";
-import doc from "@icons/doc.svg";
+import file from "@icons/file.svg";
+import flow from "@icons/flow.svg";
+import help from "@icons/help.svg";
+import home from "@icons/home.svg";
+import layout from "@icons/layout.svg";
 import lightbulb from "@icons/lightbulb.svg";
+import pdf from "@icons/pdf.svg";
+import search from "@icons/search.svg";
+import settings from "@icons/settings.svg";
+import users from "@icons/users.svg";
 import warning from "@icons/warning.svg";
-import chevronDown from "@icons/chevron-down.svg";
 
 export default {
   arrowCircle,
+  chart,
+  chevronDown,
+  clipboard,
+  doc,
+  dotsHorizontal,
   download,
   eye,
-  dotsHorizontal,
-  pdf,
+  file,
+  flow,
+  help,
+  home,
+  layout,
   lightbulb,
-  doc,
+  pdf,
+  search,
+  settings,
+  users,
   warning,
-  chevronDown,
 };
