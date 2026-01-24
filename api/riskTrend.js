cafdc9064bcc690e748542752bc07b6fbea61f94
Bogomil Stoynov <bogomil@bstoynov.com>
2026-01-24 21:46:09 +0200

    test random delays on api

--- api/riskTrend.js
+++ api/riskTrend.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -6,6 +8,8 @@ export default function handler(req, res) {
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
+
+  await delay(Math.random() * 1000);
 
   res.status(200).json({
     yStart: 0,
