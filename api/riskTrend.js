1d8dd25ba376d164c89f240274b1e333ea85f8e9
Bogomil Stoynov <bogomil@bstoynov.com>
2026-01-24 12:57:31 +0200

    change risk trend data

--- api/riskTrend.js
+++ api/riskTrend.js
@@ -12,8 +12,8 @@ export default function handler(req, res) {
     yEnd: 40,
     dataSize: 12,
     data: {
-      RiskExposure: [8, 12, 10, 15, 11, 12, 9, 14, 7, 16, 13, 10],
-      DocumentsAnalyzed: [12, 18, 22, 28, 35, 47, 32, 38, 25, 42, 30, 36],
+      RiskExposure: [20, 10, 25, 15, 22, 23, 25, 25, 10, 20, 12, 10],
+      DocumentsAnalyzed: [15, 22, 33, 25, 32, 35, 40, 28, 33, 34, 27, 37],
     },
   });
 }
